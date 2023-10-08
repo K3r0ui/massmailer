841c1c2551558249c3da2a77873bdc991051d5c4
Amine KAROUI <aminkarouidev@gmail.com>
2023-10-09 00:18:23 +0200

    Mail bul sender done with interface

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
+from werkzeug.utils import secure_filename
+import os
 app = Flask(__name__)
 app.secret_key = "dfgjliedjgldjflgjdfl"  # Change this to a random string
 
@@ -10,22 +11,42 @@ app.secret_key = "dfgjliedjgldjflgjdfl"  # Change this to a random string
 def index():
     return render_template("email_form.html")
 
+UPLOAD_FOLDER = 'C://Users/Public/Documents'
+ALLOWED_EXTENSIONS = {'txt'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route("/send_email", methods=["POST"])
 def send_email():
-    recipient = request.form.get("recipient")
-    subject = request.form.get("subject")
-    body = request.form.get("body")
+    recipient_file = request.files.get("recipient_file")
     
-    # SMTP Settings
-    # Note: Make sure to handle exceptions, log errors, and possibly inform the user
+    # Check if a file was uploaded and if it is allowed
+    if recipient_file and allowed_file(recipient_file.filename):
+        filename = secure_filename(recipient_file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        recipient_file.save(filepath)
+        
+        with open(filepath, "r") as f:
+            recipients = f.read().splitlines()
+        
+        subject = request.form.get("subject")
+        body = request.form.get("body")
 
-    # Let's assume you have a function `send_email_via_smtp` to handle the email sending.
-    try:
-        send_email_via_smtp(recipient, subject, body)
-        flash("Email sent successfully!", "success")
-    except Exception as e:
-        flash(str(e), "error")
-
+        # Send an email to each recipient
+        for recipient in recipients:
+            try:
+                send_email_via_smtp(recipient, subject, body)
+                flash("Emails sent successfully!", "success")
+            except Exception as e:
+                flash(str(e), "error")
+                return redirect(url_for("index"))
+                
+    else:
+        flash("Invalid file type. Please upload a .txt file.", "error")
+    
     return redirect(url_for("index"))
 
 def send_email_via_smtp(recipient, subject, body):
